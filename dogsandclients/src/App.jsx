9ae5fb770bcfa41517ff17d0cd9b0fd9950b2eda
Diego Zapata <diegozapata063@gmail.com>
2024-09-14 14:59:32 -0500

    feat: Structure the json file

--- dogsandclients/src/App.jsx
+++ dogsandclients/src/App.jsx
@@ -1,11 +1,43 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  const fetchDogs = () => {
+    axios.get("http://localhost:3000/dogs").then((response) => {
+      setDogs(response.data);
+      console.log(response.data);
+    });
+  };
+
+  useEffect(fetchDogs, []);
+
   return (
     <>
-
+      <h1>Dogs Information</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>Breed</th>
+            <th>age</th>
+            <th>weight</th>
+            <th>Breed</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Fido</td>
+            <td>Golden Retriever</td>
+            <td>3</td>
+            <td>65</td>
+            <td>Alice</td>
+          </tr>
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

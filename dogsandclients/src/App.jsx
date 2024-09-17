b5256f858c39f618541cd3f42bae464b6a4c5f51
Diego Zapata <diegozapata063@gmail.com>
2024-09-17 02:43:11 -0500

    feat: upgrate crud and view the medicines of pets

--- dogsandclients/src/App.jsx
+++ dogsandclients/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import './Style.css';
+import "./Style.css";
+import PetsTable from "./components/Petstable";
+import ClientsTable from "./components/Clientstable";
+import MedicinesTable from "./components/MedicinesTable";
 
 function App() {
-  const [pets, setPets] = useState([]);
   const [clients, setClients] = useState([]);
   const [medicines, setMedicines] = useState([]);
-
-  const fetchPets = () => {
-    axios.get("http://localhost:3001/dogs").then((response) => {
-      setPets(response.data);
-      console.log("pets", response.data);
-    });
-  };
+  const [selectedPets, setSelectedPets] = useState(true);
+  const [selectedClients, setSelectedClients] = useState(false);
+  const [selectedMedicines, setSelectedMedicines] = useState(false);
 
   const fetchClients = () => {
     axios.get("http://localhost:3001/clients").then((response) => {
@@ -28,106 +26,40 @@ function App() {
     });
   };
 
-  useEffect(fetchPets, []);
   useEffect(fetchClients, []);
   useEffect(fetchMedicines, []);
 
-  const handleAdd = () => {
-    const name = prompt("Enter the name of the pet");
-    const breed = prompt("Enter the breed of the pet");
-    const age = prompt("Enter the age of the pet");
-    const weight = prompt("Enter the weight of the pet");
-    const owner = prompt("Enter the owner of the pet");
-
-    axios
-      .post("http://localhost:3001/dogs", {
-        name,
-        breed,
-        age,
-        weight,
-        owner,
-      })
-      .then(() => {
-        fetchPets();
-      });
+  const selectPets = () => {
+    setSelectedPets(true);
+    setSelectedClients(false);
+    setSelectedMedicines(false);
   };
 
-  const handleEdit = (id) => {
-    const name = prompt("Enter the new name of the pet");
-    const breed = prompt("Enter the new breed of the pet");
-    const age = prompt("Enter the new age of the pet");
-    const weight = prompt("Enter the new weight of the pet");
-    const owner = prompt("Enter the new owner of the pet");
+  const selectClients = () => {
+    setSelectedPets(false);
+    setSelectedClients(true);
+    setSelectedMedicines(false);
+  };
 
-    axios
-      .put("http://localhost:3001/dogs/" + id, {
-        name,
-        breed,
-        age,
-        weight,
-        owner,
-      })
-      .then(() => {
-        fetchPets();
-      });
-  }
-
-  const handleDelete = (id) => {
-    axios.delete("http://localhost:3001/dogs/" + id).then(() => {
-      fetchPets();
-    });
-  }
+  const selectMedicines = () => {
+    setSelectedPets(false);
+    setSelectedClients(false);
+    setSelectedMedicines(true);
+  };
 
   return (
     <>
       <div className="container">
-      <h1>Pets Information</h1>
-      <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Breed</th>
-            <th>Age</th>
-            <th>Weight</th>
-            <th>Owner</th>
-            <th>Medicines</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {pets.map((pet) => {
-            const client = clients.find((client) => client.id === pet.owner);
-            return (
-              <tr key={pet.id}>
-                <td>{pet.name}</td>
-                <td>{pet.breed}</td>
-                <td>{pet.age}</td>
-                <td>{pet.weight}</td>
-                <td>
-                  {client
-                    ? client.name + " " + client.surname
-                    : "No owner found"}
-                </td>
-                <td>
-                  <a href="">See medicines</a>
-                </td>
-                <td>
-                  <button onClick={() => handleEdit(pet.id)}>Edit</button>
-                </td>
-                <td>
-                  <button onClick={() => handleDelete(pet.id)}>Delete</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      </div>
-      <button onClick={() => handleAdd}>Add Pet</button>
-      <button>See all clients</button>
-      <button>See all medicines</button>
+        {selectedPets && <PetsTable clients={clients} />}
+        {selectedClients && <ClientsTable clients={clients} fetchClients={fetchClients}/>}
+        {selectedMedicines && <MedicinesTable medicines={medicines} fetchMedicines={fetchMedicines} /> }
+        {!selectedPets && <button onClick={selectPets}> See all pets</button>}
+        {!selectedClients && (
+          <button onClick={selectClients}>See all clients</button>
+        )}
+        {!selectedMedicines && (
+          <button onClick={selectMedicines}>See all medicines</button>
+        )}
       </div>
     </>
   );
